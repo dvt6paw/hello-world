b1e14dc1c45beb2dd190fc77b292a707407b2fc9
Drew Tran <andrew.tran1@pcc.edu>
2020-02-02 13:25:39 -0800

    added main method to print hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Drew Tran on 02/02/2020
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
